ca2f3704cfe19068cc4ccbae64feb6dfa3adfb06
Abt@Win <abtinkh+github@gmail.com>
2018-04-21 22:12:49 -0700

    kl divergence

--- partition.py
+++ partition.py
@@ -12,7 +12,7 @@ class IndexPartition(object):
         self.name = name
         self.ix = file_index
         self.docnums = index_docnums
-        self.tfs = self._get_terms('body')
+        self._tfs = self._get_terms('body')
         self.ix.reader()
 
     def doc_count(self):
@@ -32,7 +32,7 @@ class IndexPartition(object):
 
     def all_terms_count(self):
         count = 0
-        for t, f in self.tfs:
+        for t, f in self._tfs:
             count += f
         return count
 
@@ -45,7 +45,10 @@ class IndexPartition(object):
 
                 tfs_list = ireader.vector_as('frequency', docnum, fieldname)
                 for tf in tfs_list:
-                    self.tfs[tf[0]] += tf[1]
+                    self._tfs[tf[0]] += tf[1]
+
+    def get_tfs(self):
+        return self._tfs.copy()
 
     def remove_doc(self, docnum, fieldname='body'):
         if docnum in self.docnums:
@@ -56,20 +59,39 @@ class IndexPartition(object):
 
                 tfs_list = ireader.vector_as('frequency', docnum, fieldname)
                 for tf in tfs_list:
-                    self.tfs[tf[0]] -= tf[1]
-                    if self.tfs[tf[0]] <= 0:
-                        if self.tfs[tf[0]] == 0:
-                            self.tfs.pop(tf[0])
+                    self._tfs[tf[0]] -= tf[1]
+                    if self._tfs[tf[0]] <= 0:
+                        if self._tfs[tf[0]] == 0:
+                            self._tfs.pop(tf[0])
                         else:
                             raise ValueError('Negative value for tf in partition {}'.format(self.name))
 
     def all_stored_fields(self):
+        with self.ix.reader() as ireader:
+            for dn in list(self.docnums):
+                yield ireader.stored_fields(dn)
+
+    def _all_stored_fields(self):
         sf = {}
         with self.ix.reader() as ireader:
             for dn in list(self.docnums):
                 sf[dn] = ireader.stored_fields(dn)
         return sf
 
+
+def avg_kl_divergence(part1: IndexPartition, part2: IndexPartition, fieldname='body') -> float:
+    part1_tfs = part1.get_tfs()
+    part2_tfs = part2.get_tfs()
+    combined_tfs = defaultdict(int)
+    for t, f in part1_tfs.items():
+        combined_tfs[t] += f
+    for t, f in part2_tfs.items():
+        combined_tfs[t] += f
+
+    sum1 = 0
+    for dfs in part1.all_stored_fields():
+        # for t in dfs[fieldname]:
+        pass
 
 def get_sorted_ids(index_reader):
     count_id = []
@@ -113,10 +135,10 @@ if __name__ == '__main__':
     ix = index.open_dir(configuration['wiki13_index'])
     cache_partition = IndexPartition(ix, set([0, 1]), 'cache')
     db_partition = IndexPartition(ix, set([2, 3]), 'db')
-    print(cache_partition.tfs)
-    print(db_partition.tfs)
+    print(cache_partition._tfs)
+    print(db_partition._tfs)
     cache_partition.remove_doc(1)
     db_partition.add_doc(1)
-    print(cache_partition.tfs)
-    print(db_partition.tfs)
-    print(db_partition.all_stored_fields())
+    print(cache_partition._tfs)
+    print(db_partition._tfs)
+    print(db_partition._all_stored_fields())
