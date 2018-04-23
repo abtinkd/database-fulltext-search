8d1485336afeac73cd157e121c9c956ec4709d97
Abtin@Mac <abtinkh+github@gmail.com>
2018-04-23 13:10:31 -0700

    sorted by "count" unlimited search

--- search.py
+++ search.py
@@ -1,15 +1,17 @@
 import whoosh.index as index
 from whoosh.qparser import QueryParser
+from whoosh import sorting
 import config
 import sys
 
 
 def search(user_query: str, index_dir_path):
-    ix = index.open_dir(index_dir_path)
+    ix = index.open_dir(index_dir_path, readonly=True)
     print('Results for query [{}] in directory [{}]'.format(user_query, index_dir_path))
     with ix.searcher() as searcher:
         query = QueryParser("body", ix.schema).parse(user_query)
-        results = searcher.search(query)
+        facet = sorting.FieldFacet('count', reverse=True)
+        results = searcher.search(query, sortedby=facet, limit=None)
         print(results)
         for res in results:
             print(res)
