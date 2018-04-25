9ea5359e64f800740436f458f6661066297f5cdf
Abtin@Mac <abtinkh+github@gmail.com>
2018-04-24 18:10:01 -0700

    partitioner

--- metrics.py
+++ metrics.py
@@ -3,10 +3,10 @@ from collections import defaultdict
 from math import log
 
 
-'''Xu, Jinxi, and W. Bruce Croft. "Cluster-based language models for distributed retrieval." 
-Proceedings of the 22nd annual international ACM SIGIR conference on Research and development in information retrieval. 
-ACM, 1999.'''
 def kl_divergence(corpus1_tfs: defaultdict, corpus2_tfs: defaultdict) -> float:
+    """Xu, Jinxi, and W. Bruce Croft. "Cluster-based language models for distributed retrieval."
+    Proceedings of the 22nd annual international ACM SIGIR conference on Research and development in
+    information retrieval. ACM, 1999."""
     kl = 0.0
     c1_terms_count = sum(corpus1_tfs.values())
     c2_terms_count = sum(corpus2_tfs.values())
@@ -19,10 +19,10 @@ def kl_divergence(corpus1_tfs: defaultdict, corpus2_tfs: defaultdict) -> float:
     return kl
 
 
-'''Huang, Anna. "Similarity measures for text document clustering." 
-Proceedings of the sixth new zealand computer science research student conference (NZCSRSC2008), 
-Christchurch, New Zealand. 2008.'''
 def avg_kl_divergence(corpus1_tfidf: defaultdict, corpus2_tfidf: defaultdict) -> float:
+    """ Huang, Anna. "Similarity measures for text document clustering."
+    Proceedings of the sixth new zealand computer science research student conference (NZCSRSC2008),
+    Christchurch, New Zealand. 2008. """
     vocab = set(corpus1_tfidf.keys())
     vocab.update(set(corpus2_tfidf.keys()))
     avg_kl = 0.0
--- partition.py
+++ partition.py
@@ -6,27 +6,38 @@ import config
 import metrics
 from math import log
 import operator
+from typing import Callable
 
 
 # A filter over Index
 class IndexPartition(object):
 
-    def __init__(self, file_index: index.FileIndex, index_docnums: list, name: str):
+    def __init__(self, file_index: index.FileIndex, index_docnums: list=None, name: str='DB'):
         self.name = name
         self.ix = file_index
-        self.docnums = set(index_docnums)
+        if index_docnums is not None:
+            self._docnums = set(index_docnums)
+        else:
+            self._docnums = self._get_all_db_ids()
         self._tfs = self._get_terms('body')
 
+    def _get_all_db_ids(self):
+        all_ids = set()
+        with self.ix.reader() as ireader:
+            for i in ireader.all_doc_ids():
+                all_ids.add(i)
+        return all_ids
+
     def doc_count(self):
-        return len(self.docnums)
+        return len(self._docnums)
 
     def _get_terms(self, fieldname='body'):
         with self.ix.reader() as ireader:
-            if not ireader.has_vector(list(self.docnums)[0], fieldname):
+            if not ireader.has_vector(list(self._docnums)[0], fieldname):
                 raise NotImplementedError('Forward index (vector) is not available for {}'.format(fieldname))
 
             tfs = defaultdict(int)
-            for dn in list(self.docnums):
+            for dn in list(self._docnums):
                 tfs_list = ireader.vector_as('frequency', dn, fieldname)
                 for tf in tfs_list:
                     tfs[tf[0]] += tf[1]
@@ -39,8 +50,8 @@ class IndexPartition(object):
         return count
 
     def add_doc(self, docnum, fieldname='body'):
-        if docnum not in self.docnums:
-            self.docnums.add(docnum)
+        if docnum not in self._docnums:
+            self._docnums.add(docnum)
             with self.ix.reader() as ireader:
                 if not ireader.has_vector(docnum, fieldname):
                     raise NotImplementedError('Forward index (vector) is not available for {}'.format(fieldname))
@@ -50,8 +61,12 @@ class IndexPartition(object):
                     self._tfs[tf[0]] += tf[1]
 
     def search(self, text: str, sorted_by_count=False, fieldname='body'):
+        """returns (dset, item_gen)
+        results.docs(): a set of docnum of the results in index
+        results.items(): a generator for (docnum, score) in from highest score/count to lowest
+        """
         with self.ix.searcher() as isearcher:
-            skw = {'limited': None}
+            skw = {'limit': None}
             skw['q'] = QueryParser("body", self.ix.schema).parse(text)
             if sorted_by_count:
                 skw['sortedby'] = sorting.FieldFacet('count', reverse=True)
@@ -65,7 +80,7 @@ class IndexPartition(object):
         dfs = defaultdict(int)
         for t in self._tfs.keys():
             docnums, _ = self.search(t)
-            dfs[t] = len(docnums.intersection(self.docnums))
+            dfs[t] = len(docnums.intersection(self._docnums))
         return dfs
 
 
@@ -73,17 +88,17 @@ class IndexPartition(object):
         tfidfs = defaultdict(float)
         dfs = self.get_dfs()
         with self.ix.reader() as ireader:
-            for t in ireader.field_terms(fieldname):
+            for t in self._tfs.keys():
                 # divide by dfs[t] as of normalization
                 tfidfs[t] = (self._tfs[t]/dfs[t]) * log(ireader.doc_count() / ireader.doc_frequency(fieldname, t))
         return tfidfs
 
     def get_docnums(self):
-        return list(self.docnums)
+        return list(self._docnums)
 
     def remove_doc(self, docnum, fieldname='body'):
-        if docnum in self.docnums:
-            self.docnums.remove(docnum)
+        if docnum in self._docnums:
+            self._docnums.remove(docnum)
             with self.ix.reader() as ireader:
                 if not ireader.has_vector(docnum, fieldname):
                     raise NotImplementedError('Forward index (vector) is not available for {}'.format(fieldname))
@@ -98,32 +113,41 @@ class IndexPartition(object):
                             raise ValueError('Negative value for tf in partition {}'.format(self.name))
 
     def all_stored_fields(self):
+        """ A generator for stored fields
+        :return:
+        {'articleID': '1', 'count': 10, 'title': 'Hi', 'xpath': '/Volumes/archive/Code/PycharmProjects/database-capacity/database-capacity/python/cache_enhancement/data/sample/sample_docs/1.txt'}
+        """
         with self.ix.reader() as ireader:
-            for dn in list(self.docnums):
+            for dn in list(self._docnums):
                 yield ireader.stored_fields(dn)
 
     def _all_stored_fields(self):
         sf = {}
         with self.ix.reader() as ireader:
-            for dn in list(self.docnums):
+            for dn in list(self._docnums):
                 sf[dn] = ireader.stored_fields(dn)
         return sf
 
     def get_popularity_distribution(self)-> list:
         pop_dist = defaultdict(int)
         with self.ix.reader() as ireader:
-            for dn in list(self.docnums):
+            for dn in list(self._docnums):
                 pop_dist[dn] = ireader.stored_fields(dn)['count']
         return sorted(pop_dist.items(), key=operator.itemgetter(1), reverse=True)
 
-    def doc_kld(self, docnum, fieldname='body'):
+    def docs_kld(self, docnums: list, fieldname: str='body'):
+        docnums = list(set(docnums))
+        dn_kld_list = {}
         with self.ix.reader() as ireader:
-            if not ireader.has_vector(docnum, fieldname):
-                raise NotImplementedError('Forward index (vector) is not available for doc {} field {}'
-                                          .format(docnum, fieldname))
+            if not ireader.has_vector(docnums[0], fieldname):
+                raise NotImplementedError('Forward index (vector) is not available for field {}'
+                                          .format(fieldname))
 
-            doc_tfs = ireader.vector_as('frequency', docnum, fieldname)
-        return distance(self.get_tfs(), doc_tfs, metrics.kl_divergence)
+            for dn in docnums:
+                d_tfs = ireader.vector_as('frequency', dn, fieldname)
+                doc_tfs = defaultdict(int, {t:f for (t,f) in d_tfs})
+                dn_kld_list[dn] = distance(doc_tfs, self.get_tfs(), metrics.kl_divergence)
+        return dn_kld_list
 
     def doc_avg_kld(self, docnum, fieldname='body'):
         with self.ix.reader() as ireader:
@@ -137,7 +161,7 @@ class IndexPartition(object):
 
 
 
-def distance(part1: defaultdict, part2: defaultdict, metric: function) -> float:
+def distance(part1: defaultdict, part2: defaultdict, metric: Callable) -> float:
     return metric(part1, part2)
 
 
@@ -155,29 +179,35 @@ def combine(part1: IndexPartition, part2: IndexPartition):
         com_part.add_doc(dn)
     return com_part
 
-def partition_popularity(index_path, threasholds=[0.9]):
-    threasholds = list(set(threasholds))
-    threasholds.sort(reverse=True)
-    ix = index.open_dir(index_path, readonly=True)
-    with ix.searcher() as isearcher:
-        isearcher.documents()
+
+
 
 class Partitioner(object):
 
     def __init__(self, index_path: str):
         self._ix = index.open_dir(index_path, readonly=True)
-        self._docnum_pop = self._docnum_pop()
+        self._pop_dn = []
+        with self._ix.reader() as ireader:
+            for dx in ireader.iter_docs():
+                self._pop_dn += [(int(dx[1]['count']), dx[0])]
+        self._pop_dn.sort(reverse=True)
 
-    def _docnum_pop(self):
-        ireader = self._ix.reader()
-        dn_pop = defaultdict(int)
-        for dx in ireader.iter_docs():
-            dn_pop[dx[0]] = int(dx[1]['count'])
-        return dn_pop
-
-    def get_sorted_ids(self):
-        return sorted(self._docnum_pop().items(), key=operator.itemgetter(1), reverse=True)
-
+    def get_partition_popularity(self, threasholds=[0.9]):
+        threasholds += [0.0]
+        threasholds = list(set(threasholds))
+        threasholds.sort(reverse=True)
+        tot = len(self._pop_dn)
+        c = 0
+        ti = 0
+        docnums = []
+        for pdn in self._pop_dn:
+            c += 1
+            docnums += [pdn[1]]
+            if c > (1.0 - threasholds[ti]) * tot:
+                part = IndexPartition(self._ix, docnums, 'part'+str(ti))
+                docnums = []
+                ti += 1
+                yield part
 
 
 # def partition_popularity_based(index_path, low_pop_ratio, high_pop_ratio=1.0):
@@ -210,10 +240,20 @@ class Partitioner(object):
 
 if __name__ == '__main__':
     configuration = config.get()
+    pa = Partitioner(configuration['wiki13_index'])
+    k = pa.get_partition_popularity([0.9])
+    for kk in k:
+        print(kk.name)
+        for f in kk.all_stored_fields():
+            print(f)
+    input()
     # partition_popularity_based(configuration['wiki13_index'])
-    ix = index.open_dir(configuration['wiki13_index'])
-    cache_partition = IndexPartition(ix, [0, 1], 'cache')
-    db_partition = IndexPartition(ix, [2, 3], 'db')
+    ix = index.open_dir(configuration['wiki13_index'], readonly=True)
+    whole_db = IndexPartition(ix)
+    cache_partition = IndexPartition(ix, [0], 'cache')
+    db_partition = IndexPartition(ix, [2, 3, 1], 'rest')
+    print(cache_partition.docs_kld([2,3,1]))
+    input()
     print(cache_partition._tfs)
     print(db_partition._tfs)
     cache_partition.remove_doc(1)
