e9d19efca928c9e04273210d006778c452b7aa13
Abtin@Mac <abtinkh+github@gmail.com>
2018-04-27 18:24:28 -0700

    descriptor save

--- enhancer/describe.py
+++ enhancer/describe.py
@@ -2,30 +2,25 @@ from partition import IndexVirtualPartition
 from collections import defaultdict
 import operator
 from time import time
+import logging
+import config
 
-LOG = True
+config.setup_logger('Descriptor')
+LOGGER = logging.getLogger('Descriptor')
 
 
 class PartitionDescriptor(object):
 
     def __init__(self, this_partition: IndexVirtualPartition, cross_partition: IndexVirtualPartition):
-        st = time()
         self._partition = this_partition
         self._cross_partition = cross_partition
-        self._ixreader = this_partition.ix.reader()
-        # self._ixsearcher = this_partition.ix.searcher()
+        self._ixreader = this_partition._reader
+        self._ixsearcher = this_partition._searcher
         self.name = this_partition.name + '_descriptor'
         self.pop_distrib = None
         self.kld_distrib = None
         self.cross_kld_distrib = None
         self.update()
-
-        if LOG:
-            print('{} is initialized. [{:.4f}s]'.format(self.name, time()-st))
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self._ixreader.close()
-        # self._ixsearcher.close()
 
     def update(self, mode='all'):
         if mode == 'all' or 'pop':
@@ -42,8 +37,7 @@ class PartitionDescriptor(object):
             pop_dist[dn] = self._ixreader.stored_fields(dn)['count']
         self.pop_distrib = pop_dist
 
-        if LOG:
-            print('{} popularity distribution is updated. [{:.4f}s]'.format(self.name, time()-st))
+        LOGGER.debug('{} popularity distribution is updated. [{:.4f}s]'.format(self.name, time()-st))
 
     def get_sorted(self, mode='kld', reverse=True):
         if mode == 'pop':
@@ -61,5 +55,17 @@ class PartitionDescriptor(object):
         else:
             self.cross_kld_distrib = self._cross_partition.docs_kld(self._partition.get_docnums(), 'body')
 
-        if LOG:
-            print('{} kld distribution is updated. [{:.4f}s]'.format(self.name, time()-st))
+        LOGGER.debug('{} kld distribution is updated. [{:.4f}s]'.format(self.name, time()-st))
+
+    def save(self, file_path=None):
+        if file_path is None:
+            file_path = 'log/{}.log'.format(self.name)
+        pop_distrib = self.get_sorted('pop')
+        with open(file_path, 'w') as w:
+            w.write(self.name+' :: docnum, popularity, kld, kld_cross, aritcleId, count, xpath\n')
+            for docnum, pop in pop_distrib:
+                sf = self._ixreader.stored_fields(docnum)
+                w.write('{},{},{},{},{},{},{}\n'
+                        .format(docnum, pop, self.kld_distrib[docnum],
+                                self.cross_kld_distrib[docnum], sf['articleID'], sf['count'], sf['xpath']))
+
