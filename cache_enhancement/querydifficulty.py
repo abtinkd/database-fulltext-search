8bb788e7c8f54691ec1c6d173190a1ff201e93da
Abt@Win <abtinkh+github@gmail.com>
2018-05-21 23:41:12 -0700

    clarity

--- cache_enhancement/querydifficulty.py
+++ cache_enhancement/querydifficulty.py
@@ -8,6 +8,7 @@ import whoosh.analysis as analysis
 from math import log
 from whoosh.reading import IndexReader
 import logging
+from functools import reduce
 
 LOGGER = logging.getLogger()
 
@@ -30,8 +31,9 @@ def specificity(tf_query: defaultdict, total_query_terms: int,
     return scs
 
 
-def similarity(query_terms: list, collection: IndexVirtualPartition, mode='avg') -> float:
+def similarity(query: str, collection: IndexVirtualPartition, mode='avg') -> float:
     # mode is in {'avg', 'max', 'sum'}
+    query_terms = list(tokenize(query).keys())
     idfs = collection.get_tfidfs()
     tfs = collection.get_tfs()
     if mode == 'avg':
@@ -47,6 +49,38 @@ def similarity(query_terms: list, collection: IndexVirtualPartition, mode='avg')
     if mode == 'max':
         scq = [(1 + log(tfs[term])) * idfs[term] for term in query_terms]
         return max(scq)
+
+
+def clarity(query: str, query_results_docnum: list, collection: IndexVirtualPartition, fieldname='body'):
+    query_terms = list(tokenize(query).keys())
+    collection_tfs = collection.get_tfs()
+    collection_total_terms = collection.get_total_terms()
+    vocabulary = list(collection_tfs.keys())
+
+    def get_prob_t_condition_Dq(t: str, lambd=0.9) -> float:
+        prob_t_condit_D = collection_tfs[t] / collection_total_terms
+        norm = 0.0
+        for d in query_results_docnum:
+            norm += reduce(lambda tm, y: (tf_d[tm]/tot_d)*y, query_terms)
+        prob = 0.0
+        for dn in query_results_docnum:
+            if collection._reader.has_vector(dn, fieldname):
+                tf_d = dict(collection._reader.vector_as('frequency', dn, fieldname))
+                tot_d = sum(tf_d.values())
+                prob_t_condit_d = lambd * (tf_d[t]/tot_d) + (1-lambd) * prob_t_condit_D
+                prob_q_condit_d = reduce(lambda tm, y: (tf_d[tm]/tot_d)*y, query_terms)
+                prob_d_condit_q = prob_q_condit_d / norm
+                prob += prob_t_condit_d * prob_d_condit_q
+            else:
+                LOGGER.warning('No forward vector was found for query {} on docnum {}'.format(query, dn))
+        return prob
+
+    clt = 0.0
+    for t in vocabulary:
+        prob_t_condit_D = collection_tfs[t] / collection_total_terms
+        prob_t_condit_Dq = get_prob_t_condition_Dq(t)
+        clt += prob_t_condit_Dq * log(prob_t_condit_Dq / prob_t_condit_D)
+    return clt
 
 
 if __name__ == '__main__':
