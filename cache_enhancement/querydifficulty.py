2aa73f212a5c908a5546057e6bb21135d150da31
Abt@Win <abtinkh+github@gmail.com>
2018-05-23 21:40:00 -0700

    handling terms that don't appear in the database

--- cache_enhancement/querydifficulty.py
+++ cache_enhancement/querydifficulty.py
@@ -78,23 +78,25 @@ def similarity(query: str, collection: IndexVirtualPartition, mode='avg') -> flo
 
 
 def clarity(query: str, query_result_docs_tfs: defaultdict(lambda: defaultdict(int)),
-            collection_tfs: defaultdict, collection_total_terms: int) -> float:
+            collection_tfs: defaultdict, collection_total_terms: int, vocabulary: list=None) -> float:
     '''
     Clarity
     :param query: query text
     :param query_result_docs_tfs: {articleId1: {'term1': count1, 'term2': count2, ...}, articleId2: {...}}
     :param collection_tfs: {'term1': count1, ...}
-    :param collection_total_terms:
+    :param collection_total_terms: number of terms in the collection
+    :param vocabulary: all distinct terms in the collection
     :return: a float number
     '''
     query_terms = list(tokenize(query).keys())
-    # vocabulary = list(collection_tfs.keys())
+    if vocabulary is None:
+        vocabulary = query_terms
 
     def get_prob_t_condition_Dq(t: str, lambd=0.9) -> float:
         prob_t_condit_D = collection_tfs[t] / collection_total_terms
         prob_term_condit_doc = lambda term, tfd: lambd * (tfd[term] / sum(tfd.values())) + \
                                                          (1 - lambd) * prob_t_condit_D
-        norm = 0.0
+        norm = 0.00000001
         for tf_d in query_result_docs_tfs.values():
             norm += reduce(lambda x, y: x*y, map(lambda x: prob_term_condit_doc(x, tf_d), query_terms))
         prob = 0.0
@@ -107,7 +109,9 @@ def clarity(query: str, query_result_docs_tfs: defaultdict(lambda: defaultdict(i
         return prob
 
     clt = 0.0
-    for t in query_terms:
+    for t in vocabulary:
+        if collection_tfs[t] == 0:
+            collection_tfs[t] = 1
         prob_t_condit_D = collection_tfs[t] / collection_total_terms
         prob_t_condit_Dq = get_prob_t_condition_Dq(t)
         clt += prob_t_condit_Dq * log(prob_t_condit_Dq / prob_t_condit_D)
@@ -126,12 +130,15 @@ if __name__ == '__main__':
     LOGGER.info('Index path: ' + index_path)
     ix_reader = ix.reader()
 
+    vocabulary = []
     db_tfs = defaultdict(int)
+    db_total_terms = 0
     with open(c['db_tfs'], 'r') as fr:
         for line in fr:
             parts = line.rsplit(',', 1)
             db_tfs[parts[0]] = int(parts[1])
-    db_total_terms = sum(db_tfs.values())
+            db_total_terms += int(parts[1])
+            vocabulary.append(parts[0])
 
     df = pd.read_csv(query_file_path)
     uniqueries = df['query'].unique()
@@ -142,11 +149,12 @@ if __name__ == '__main__':
 
         aids = list(map(lambda x: str(x), qdf['articleId'].values))
         docs_tfs = get_docs_tfs(aids, ix_reader)
-        clt = clarity(q, docs_tfs, db_tfs, db_total_terms)
+        clt = clarity(q, docs_tfs, db_tfs, db_total_terms, vocabulary)
 
         df.loc[qdf.index, 'specificity'] = scs
         df.loc[qdf.index, 'clarity'] = clt
-        print(clt, scs)
+        print(scs, clt)
+        LOGGER.info('Query: {}, specificity: {}, clarity: {}'.format(q, scs, clt))
 
     ix_reader.close()
     df.to_csv(save_path, index=False)
